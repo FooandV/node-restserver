9e0d336fc6f4075af3882d20c2b83e928f7d8a28
Freyder Otalvaro <frotalva@bancolombia.com.co>
2019-11-18 22:01:06 -0500

    change config

--- server/config/config.js
+++ server/config/config.js
@@ -16,12 +16,12 @@ process.env.NODE_ENV = process.env.NODE_ENV || "dev";
 
 let urlDB;
 
-// if (process.env.NODE_ENV == 'dev'){
-//     urlDB = 'mongodb://localhost:27017/cafe'
-// }else{
+if (process.env.NODE_ENV == 'dev'){
+    urlDB = 'mongodb://localhost:27017/cafe'
+}else{
 urlDB =
   "mongodb+srv://freyder:violetta@cluster0-rzoqa.mongodb.net/test?retryWrites=true&w=majority";
-// }
+}
 
 process.env.urlDB = urlDB;
 
